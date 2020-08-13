5d487b45531c4ab3c1a087aed050d674ccecdea2
wliu <wliu@interactions.com>
2020-08-13 13:40:23 -0400

    add support for other vectorizers

--- api-examples/response_selection_map.py
+++ api-examples/response_selection_map.py
@@ -9,10 +9,12 @@ from baseline.pytorch.embeddings import *
 import baseline.embeddings
 from eight_mile.optz import *
 from eight_mile.pytorch.layers import save_checkpoint, init_distributed
+from eight_mile.downloads import EmbeddingDownloader
 from eight_mile.pytorch.optz import *
 from eight_mile.progress import create_progress_bar
 from transformer_utils import (
     MultiFileDatasetReader,
+    NextTurnPredictionFileLoader,
     get_lr_decay,
     AllLoss,
 )
@@ -30,7 +32,7 @@ class MAPModel(nn.Module):
         super().__init__()
         self.embedding_layers = encoder
         self.d_model = encoder.get_dsz()
-        self.top_layer = pytorch_linear(self.d_model, self.d_model, bias=False)
+        self.top_layer = pytorch_linear(self.d_model, self.d_model, bias=True)
         self.dropout = dropout
         self.weight_std = weight_std
         self.apply(self.init_layer_weights)
@@ -59,7 +61,7 @@ class MAPModel(nn.Module):
         return encoded_query, encoded_response
 
     def create_loss(self):
-        return AllLoss(self, warmup_steps=200)
+        return AllLoss(self, warmup_steps=100)
 
 
 def run():
@@ -69,6 +71,8 @@ def run():
     parser.add_argument("--test_file", type=str, required=True, help='File path to use for valid file')
     parser.add_argument("--embeddings", type=str, help="MEAD embedding config file")
     parser.add_argument("--embedding_label", type=str, help="label of the embedding to use")
+    parser.add_argument("--vecs", type=str, help="MEAD vecs config file")
+    parser.add_argument("--vec_label", type=str, help="label of the vectorizer to use")
     parser.add_argument("--nctx", type=int, default=64, help="Max input length")
     parser.add_argument("--pattern", default='*.txt', help="Glob pattern for data")
     parser.add_argument("--batch_size", type=int, default=100,
@@ -76,14 +80,12 @@ def run():
                              "response in top n from batch size number of candidates")
     parser.add_argument("--top_n", type=int, default=1,
                         help="if the true response is ranked top n, it will be considered correct in calculating acc")
-    parser.add_argument("--subword_model_file", type=str, help="The BPE model file", required=True)
-    parser.add_argument("--subword_vocab_file", type=str, help="The BPE subword vocab", required=True)
     parser.add_argument("--dropout", type=float, default=0.0, help="Dropout")
     parser.add_argument("--lr_scheduler", type=str, default='cosine', help="The type of learning rate decay scheduler")
     parser.add_argument("--lr_decay_steps", type=int, help="decay steps of lr scheduler")
     parser.add_argument("--lr_decay_rate", type=float, help="decay rate of lr scheduler")
     parser.add_argument("--lr_alpha", type=float, default=0.1, help="parameter alpha for cosine decay scheduler")
-    parser.add_argument("--warmup_steps", type=int, default=20, help="Num warmup steps")
+    parser.add_argument("--warmup_steps", type=int, default=100, help="Num warmup steps")
     parser.add_argument("--optim", default="adamw", type=str, help="Optimizer to use (defaults to adamw)")
     parser.add_argument("--lr", type=float, default=4.0e-5, help="Learning rate")
     parser.add_argument("--clip", type=float, default=1.0, help="Clipping gradient norm")
@@ -95,21 +97,28 @@ def run():
     parser.add_argument("--output", type=str, help="write results to a file", default='./results.txt')
     args = parser.parse_args()
 
+    vecs = read_config_stream(args.vecs)
+    vecs_set = index_by_label(vecs)
+    vecs_config = vecs_set[args.vec_label]
+    vectorizer = baseline.vectorizers.create_vectorizer(vectorizer_type=vecs_config['type'], **vecs_config)
+
     embeddings = read_config_stream(args.embeddings)
     embeddings_set = index_by_label(embeddings)
     embeddings_config = embeddings_set[args.embedding_label]
-
+    embed_file = embeddings_config.get('file')
+    unzip_file = embeddings_config.get('unzip', True)
+    embed_dsz = embeddings_config['dsz']
+    embed_sha1 = embeddings_config.get('sha1')
+    embed_file = EmbeddingDownloader(embed_file, embed_dsz, embed_sha1, os.path.expanduser("~/.bl-data"), unzip_file=unzip_file).download()
     preproc_data = baseline.embeddings.load_embeddings('encoder',
                                                        embed_type=embeddings_config['type'],
-                                                       embed_file=embeddings_config['file'],
+                                                       embed_file=embed_file,
                                                        **embeddings_config['model'])
     encoder = preproc_data['embeddings']
     vocab = preproc_data['vocab']
 
-    reader = MultiFileDatasetReader(args.nctx, args.subword_model_file, args.subword_vocab_file, args.pattern,
-                                    reader_type='ntp', vectorizer_begin_tok='[CLS]', vectorizer_end_tok='<EOU>')
-    train_set = reader.load(args.train_file, vocab, distribute=False, shuffle=True)
-    test_set = reader.load(args.test_file, vocab, distribute=False, shuffle=False)
+    train_set = NextTurnPredictionFileLoader(args.train_file, args.pattern, vocab, vectorizer, args.nctx, distribute=False, shuffle=True)
+    test_set = NextTurnPredictionFileLoader(args.test_file, args.pattern, vocab, vectorizer, args.nctx, distribute=False, shuffle=False)
     train_loader = DataLoader(train_set, batch_size=args.batch_size)
     test_loader = DataLoader(test_set, batch_size=args.batch_size)
     train_steps = len(train_loader) // args.batch_size
@@ -160,9 +169,10 @@ def run():
         # This is the token-level training perplexity
         metrics['train_elapsed_min'] = elapsed
         metrics['average_train_loss'] = train_avg_loss
+        metrics['lr'] = optimizer.current_lr
         logger.info(metrics)
 
-        # testing
+    # testing
         numerator = 0
         denominator = 0
         metrics = {}
